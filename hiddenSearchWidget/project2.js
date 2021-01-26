3906626dd6a3412a2cdf87aa2918e45d45a5f634
Fatima Abdimalik <f.abdimalik@gmail.com>
2021-01-26 23:19:05 +0000

    added js

--- hiddenSearchWidget/project2.js
+++ hiddenSearchWidget/project2.js
@@ -1,1 +1,11 @@
-console.log("HI challenge 2");
+const searchButtton = document.getElementById("style");
+console.log(searchButtton);
+const searchField = document.getElementById("expand");
+console.log(searchField);
+
+const expnadSearchField = () => {
+  console.log("clicked");
+  searchField.style.opacity = 1;
+};
+
+searchButtton.addEventListener("click", expnadSearchField);
