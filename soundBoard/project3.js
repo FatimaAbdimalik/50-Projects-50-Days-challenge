98f61041e644c3dbb6d59f336f7e548c09dd428f
Fatima Abdimalik <f.abdimalik@gmail.com>
2021-01-27 19:57:40 +0000

    removed all commendedlines

--- soundBoard/project3.js
+++ soundBoard/project3.js
@@ -1,30 +1,5 @@
 // play() can only play audio, therefore, give the id to audio not the src
-
-// const applauseSound = document.getElementById("applause");
-// const booSound = document.getElementById("boo");
-
-// console.log(applauseSound);
-// console.log(booSound);
-
-// const applauseButton = document.getElementById("applause-btn");
-// const booButton = document.getElementById("boo-btn");
-// console.log(booButton);
-
-// console.log(applauseButton);
-
-// const playApplauses = () => {
-//   console.log("clicked");
-//   applauseSound.play();
-// };
-
-// const playBoo = () => {
-//   booSound.play();
-// };
-// applauseButton.addEventListener("click", playApplauses);
-
-// booButton.addEventListener("click", playBoo);
-
-const soundArray = ["Applause", "Boo", "Gasp", "Tada"];
+const soundArray = ["Applause", "Boo", "Gasp", "Tada", "Victory", "Wrong"];
 const mainDiv = document.getElementById("main");
 
 for (let i = 0; i < soundArray.length; i++) {
