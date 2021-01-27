30de23a29e27f06c03d1f17c1300c31b2d7c5748
Fatima Abdimalik <f.abdimalik@gmail.com>
2021-01-27 19:04:06 +0000

    sounds won't overlap

--- soundBoard/project3.js
+++ soundBoard/project3.js
@@ -1,25 +1,50 @@
 // play() can only play audio, therefore, give the id to audio not the src
 
-const applauseSound = document.getElementById("applause");
-const booSound = document.getElementById("boo");
+// const applauseSound = document.getElementById("applause");
+// const booSound = document.getElementById("boo");
 
-console.log(applauseSound);
-console.log(booSound);
+// console.log(applauseSound);
+// console.log(booSound);
 
-const applauseButton = document.getElementById("applause-btn");
-const booButton = document.getElementById("boo-btn");
-console.log(booButton);
+// const applauseButton = document.getElementById("applause-btn");
+// const booButton = document.getElementById("boo-btn");
+// console.log(booButton);
 
-console.log(applauseButton);
+// console.log(applauseButton);
 
-const playApplauses = () => {
-  console.log("clicked");
-  applauseSound.play();
+// const playApplauses = () => {
+//   console.log("clicked");
+//   applauseSound.play();
+// };
+
+// const playBoo = () => {
+//   booSound.play();
+// };
+// applauseButton.addEventListener("click", playApplauses);
+
+// booButton.addEventListener("click", playBoo);
+
+const soundArray = ["Applause", "Boo", "Gasp", "Tada"];
+const mainDiv = document.getElementById("main");
+
+for (let i = 0; i < soundArray.length; i++) {
+  let sound = document.createElement("h5");
+  sound.innerHTML = soundArray[i];
+  console.log(sound.innerHTML);
+  mainDiv.appendChild(sound);
+
+  sound.addEventListener("click", () => {
+    //stop already playing sound
+    stopSound();
+    document.getElementById(sound.innerHTML.toLowerCase()).play();
+  });
+}
+
+const stopSound = () => {
+  soundArray.forEach((item) => {
+    const song = document.getElementById(item.toLowerCase());
+    console.log(song);
+    song.pause();
+    song.currentTime = 0;
+  });
 };
-
-const playBoo = () => {
-  booSound.play();
-};
-applauseButton.addEventListener("click", playApplauses);
-
-booButton.addEventListener("click", playBoo);
