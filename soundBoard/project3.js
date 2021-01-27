57eee0202e42fa2f594250cbf8846592eef55365
Fatima Abdimalik <f.abdimalik@gmail.com>
2021-01-27 15:34:44 +0000

    added another sound

--- soundBoard/project3.js
+++ soundBoard/project3.js
@@ -1,10 +1,14 @@
 // play() can only play audio, therefore, give the id to audio not the src
 
 const applauseSound = document.getElementById("applause");
+const booSound = document.getElementById("boo");
 
 console.log(applauseSound);
+console.log(booSound);
 
 const applauseButton = document.getElementById("applause-btn");
+const booButton = document.getElementById("boo-btn");
+console.log(booButton);
 
 console.log(applauseButton);
 
@@ -13,4 +17,9 @@ const playApplauses = () => {
   applauseSound.play();
 };
 
+const playBoo = () => {
+  booSound.play();
+};
 applauseButton.addEventListener("click", playApplauses);
+
+booButton.addEventListener("click", playBoo);
